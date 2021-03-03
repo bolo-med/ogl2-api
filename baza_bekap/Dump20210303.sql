2f0fa5881e232b0dffb75cdbe6d61dff0ce441d0
bolo-med <bosko.medojevic@t-com.me>
2021-03-03 15:36:51 +0100

    sitnica

--- baza_bekap/Dump20210303.sql
+++ baza_bekap/Dump20210303.sql
@@ -28,7 +28,7 @@ CREATE TABLE `kategorije` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `naziv` char(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `kategorije` (
 
 LOCK TABLES `kategorije` WRITE;
 /*!40000 ALTER TABLE `kategorije` DISABLE KEYS */;
-INSERT INTO `kategorije` VALUES (2,'Vozila'),(3,'Nekretnine'),(4,'Mobilni telefoni'),(6,'proba222'),(7,'Razno');
+INSERT INTO `kategorije` VALUES (2,'Vozila'),(3,'Nekretnine'),(4,'Mobilni telefoni'),(7,'Razno'),(38,'proba3_izmjena');
 /*!40000 ALTER TABLE `kategorije` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,17 +110,25 @@ DROP TABLE IF EXISTS `oglasi`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `oglasi` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `naslov` varchar(45) NOT NULL,
   `tekst` text NOT NULL,
+  `foto01` varchar(50) DEFAULT NULL,
+  `foto02` varchar(50) DEFAULT NULL,
+  `foto03` varchar(50) DEFAULT NULL,
   `dat_objave` date NOT NULL,
   `dat_vazenja` date DEFAULT NULL,
+  `arhiviran` int(11) NOT NULL,
   `kategorija_id` int(11) NOT NULL,
+  `potkategorija_id` int(11) NOT NULL,
   `korisnik_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `kategorija_id` (`kategorija_id`),
   KEY `korisnik_id` (`korisnik_id`),
+  KEY `potkategorija_id` (`potkategorija_id`),
   CONSTRAINT `oglasi_ibfk_1` FOREIGN KEY (`kategorija_id`) REFERENCES `kategorije` (`id`),
-  CONSTRAINT `oglasi_ibfk_2` FOREIGN KEY (`korisnik_id`) REFERENCES `korisnici` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  CONSTRAINT `oglasi_ibfk_2` FOREIGN KEY (`korisnik_id`) REFERENCES `korisnici` (`id`),
+  CONSTRAINT `oglasi_ibfk_3` FOREIGN KEY (`potkategorija_id`) REFERENCES `podkategorije` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -129,8 +137,35 @@ CREATE TABLE `oglasi` (
 
 LOCK TABLES `oglasi` WRITE;
 /*!40000 ALTER TABLE `oglasi` DISABLE KEYS */;
-INSERT INTO `oglasi` VALUES (1,'probni tekst oglasa 1','2020-11-06','2020-11-10',4,11),(3,'probni tekst oglasa 3','2020-11-06','2020-11-10',4,11),(4,'probni tekst oglasa 444','2020-11-06','2020-11-10',4,11),(5,'Probni tekst oglasa.','2020-11-27',NULL,2,12),(6,'Probni tekst 2.','2020-11-27',NULL,6,12),(7,'Tekst. Proba.','2020-11-27',NULL,6,12);
+INSERT INTO `oglasi` VALUES (1,'Neki naslov','probni tekst oglasa 1',NULL,NULL,NULL,'2020-11-06','2020-11-10',0,4,7,11),(3,'Neki naslov','probni tekst oglasa 3',NULL,NULL,NULL,'2020-11-06','2020-11-10',1,4,11,11),(4,'Neki naslov','probni tekst oglasa 444',NULL,NULL,NULL,'2020-11-06','2020-11-10',0,4,7,11),(5,'Neki naslov','Probni tekst oglasa.',NULL,NULL,NULL,'2020-11-27',NULL,0,2,1,12),(6,'Neki naslov','Probni tekst 2.',NULL,NULL,NULL,'2020-11-27',NULL,1,38,6,12),(7,'Neki naslov','Tekst. Proba.',NULL,NULL,NULL,'2020-11-27',NULL,0,38,6,12),(12,'BMW X5, 2018. godiste','eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeee eeeeeeeee eeeeeeee eeeeee eeeeeeeeee eeeeeee eeeeeeeeee eeeeeeeeee eeeeeeeeeee eeeeeeeeeee eeeee eeeeeee eeeeeee eeeeeeeee e e eeeeeeeeeeeeeeeeeeeeee eeeeee',NULL,NULL,NULL,'2021-02-16',NULL,0,2,3,11),(13,'Zgrada, proba','wwwwwww ww wwwwwwww wwwww','1614693862087-zgrada.png',NULL,'1614693913949-zgradaM.png','2021-03-02',NULL,0,3,12,11),(14,'Zgrada, mala slika, proba','ww wwwwwwwwwww wwwwww','1614781008975-zgradaMM.png',NULL,NULL,'2021-03-03',NULL,0,3,12,12);
 /*!40000 ALTER TABLE `oglasi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `podkategorije`
+--
+
+DROP TABLE IF EXISTS `podkategorije`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `podkategorije` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kategorija_id` int(11) NOT NULL,
+  `naziv` char(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `kategorija_id` (`kategorija_id`),
+  CONSTRAINT `podkategorije_ibfk_1` FOREIGN KEY (`kategorija_id`) REFERENCES `kategorije` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `podkategorije`
+--
+
+LOCK TABLES `podkategorije` WRITE;
+/*!40000 ALTER TABLE `podkategorije` DISABLE KEYS */;
+INSERT INTO `podkategorije` VALUES (1,2,'sedan'),(2,2,'hatchback'),(3,2,'SUV'),(6,7,'Razno'),(7,4,'Smart'),(11,4,'Feature'),(12,3,'Stan'),(13,3,'Plac');
+/*!40000 ALTER TABLE `podkategorije` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -142,4 +177,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-01 16:09:37
+-- Dump completed on 2021-03-03 15:32:25
